e6c9d3a4b65bb629b7e3a56b357d8b698731f9ab
Cristian Nieto <cristiandeveloper4699@gmail.com>
2024-06-20 00:33:16 -0500

    Server port changed

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@
 const path = require('path');
 const express = require("express");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
