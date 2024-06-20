825957d4417e6d07dd897e700f8b80c713e1d9e1
Cristian Nieto <cristiandeveloper4699@gmail.com>
2024-06-19 23:47:24 -0500

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+// server/index.js
+
+const path = require('path');
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
+
+app.get("/api", (req, res) => {
+    res.json({message: "Hola desde el servidor!"});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${ PORT }`);
+}); 
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
+});
