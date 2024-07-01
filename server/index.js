d4a28100bd2df00e0f90c233e0580fc9a67705bb
Cristian Nieto <cristiandeveloper4699@gmail.com>
2024-07-01 13:28:27 -0500

    React server created

--- server/index.js
+++ server/index.js
@@ -1,3 +1,58 @@
+const path = require('path');
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get("/api", (req,res) => {
+    res.json({ message: "Hola desde el servidor!" });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // server/index.js
 
 const path = require('path');
@@ -19,3 +74,4 @@ app.listen(PORT, () => {
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
 });
+*/
